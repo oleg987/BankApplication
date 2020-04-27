7bf671214b0143c6ffaa8c5a2f5de56ebb5a2918
oleg fruntov <fruntovoleg@gmail.com>
2020-04-27 18:36:55 +0300

    Добавьте файлы проекта.

---
+++ BankApplication/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BankApplication.Models;
+
+namespace BankApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Bank bank = new Bank();
+
+            return View(bank);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Page()
+        {
+            List<Bank> banks = new List<Bank>();
+
+            banks.Add(new Bank());
+            banks.Add(new Bank() { Title = "GigaBank"});
+            banks.Add(new Bank() { Title = "TeraBank"});
+
+            return View(banks);
+        }
+    }
+}
---
+++ BankApplication/Models/Bank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApplication.Models
+{
+    public class Bank
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+
+        public Bank()
+        {
+            Id = 1;
+            Title = "MegaBank";
+            Address = "WallStreet 1";
+        }
+    }
+}
